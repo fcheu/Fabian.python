9f3290192501297ab91a664b2dbce1eaed050c11
fcheu <fabian.cheuquerojas@gmail.com>
2026-04-30 15:51:45 -0400

    clase_3004

--- 07/prueba.py
+++ 07/prueba.py
@@ -27,43 +27,67 @@
 # # ademas pida el pin que debe ser 3435
 # # los dos deben estar correctos para acceder al sistema
 
-passw="temu"
-pin=3435
+# passw="temu"
+# pin=3435
 
-palabra=input("Ingrese la palabra secreta: ")
-code=int(input("Ingrese el pin de 4 digitos: "))
+# palabra=input("Ingrese la palabra secreta: ")
+# code=int(input("Ingrese el pin de 4 digitos: "))
 
-if code==pin and passw==palabra:
-    print("Acceso concedido")
-else:
-    print("Algo salio mal")
+# if code==pin and passw==palabra:
+#     print("Acceso concedido")
+# else:
+#     print("Algo salio mal")
 
-ingreso=int(input("Ingrese su sueldo: "))
+# ingreso=int(input("Ingrese su sueldo: "))
 
-print("1.- Básico")
-print("2.- Medio")
-print("3.- Superior")
-edu=int(input("Ingrese su nivel educacional: "))
-nacionalidad=input("Ingrese nacionalidad (chilena/ otra)")
-credito=0
-if ingreso>500000 and ingreso<=1000000:
-    credito=credito+300000
-elif ingreso>1000001 and ingreso<=1500000:
-    credito=credito+650000
-elif ingreso>1500001 :
-    credito=credito+1000000
-else:
-    print("no tiene sueldo suficiente")
-if edu==1:
-    print("NO TIENE BENEFICIO POR EDUCACION")
-elif edu==2:
-    credito=credito*1.3
-elif edu==3:
-    credito=credito*1.5
+# print("1.- Básico")
+# print("2.- Medio")
+# print("3.- Superior")
+# edu=int(input("Ingrese su nivel educacional: "))
+# nacionalidad=input("Ingrese nacionalidad (chilena/ otra)")
+# credito=0
+# if ingreso>500000 and ingreso<=1000000:
+#     credito=credito+300000
+# elif ingreso>1000001 and ingreso<=1500000:
+#     credito=credito+650000
+# elif ingreso>1500001 :
+#     credito=credito+1000000
+# else:
+#     print("no tiene sueldo suficiente")
+# if edu==1:
+#     print("NO TIENE BENEFICIO POR EDUCACION")
+# elif edu==2:
+#     credito=credito*1.3
+# elif edu==3:
+#     credito=credito*1.5
 
-if nacionalidad=="chilena":
-    credito=credito+300000
-else:
-    print("No tiene bono por nacionalidad")
+# if nacionalidad=="chilena":
+#     credito=credito+300000
+# else:
+#     print("No tiene bono por nacionalidad")
 
-print("Su puntaje de credito es : ", credito)
+# print("Su puntaje de credito es : ", credito)
+import random
+
+jugador1=input("Ingrese el nombre del jugador 1:")
+jugador2=input("Ingrese el nombre del jugador 2:")
+j1=100
+j2=100
+turno=1
+while j1>0 and j2>0:
+    punch=random.randint(7,18)
+    if turno%2==1:
+        print("Turno uno")
+        print(jugador1, "hizo", punch, "de daño")
+        print(jugador2, "tiene", j2-punch, "de HP")
+
+    else:
+        print("Turno dos")
+        print(jugador2, "hizo", punch, "de daño")
+        print(jugador1, "tiene", j1-punch, "de HP")
+    turno+=1
+
+if j1<=0:
+    print(jugador2, "ganó")
+elif j2<=0:
+    print(jugador1, "ganó")
--- 30_04/ramdomly.py
+++ 30_04/ramdomly.py
@@ -46,19 +46,68 @@ import random
 # cada persona tiene la posibilidad de golpear
 # y la distancia varia entre 60 y 190
 # mostrar al final, el golpe mas fuerte 
+import time
+# jugador1=random.randint(60,190)
+# jugador2=random.randint(60,190)
+# jugador3=random.randint(60,190)
+# time.sleep(1)
 
-jugador1=random.randint(60,190)
-jugador2=random.randint(60,190)
-jugador3=random.randint(60,190)
+# print("El jugador 1 golpeó", jugador1,"metros")
+# print("El jugador 2 golpeó", jugador2,"metros")
+# print("El jugador 3 golpeó", jugador3,"metros")
+# time.sleep(3)
 
-print("El jugador 1 golpeó", jugador1,"metros")
-print("El jugador 2 golpeó", jugador2,"metros")
-print("El jugador 3 golpeó", jugador3,"metros")
+# if jugador1>jugador2 and jugador1>jugador3:
+#     print("El jugador 1 tuvo el golpe mas fuerte, que fue de:", jugador1,"metros")
+# elif jugador2>jugador1 and jugador2>jugador3:
+#     print("El jugador 2 tuvo el golpe mas fuerte, que fue de:", jugador2, "metros")
+# else:
+#     print("El jugador 3 tuvo el golpe mas fuerte, que fue de:", jugador3, "metros")
 
-if jugador1>jugador2 and jugador1>jugador3:
-    print("El jugador 1 tuvo el golpe mas fuerte, que fue de:", jugador1,"metros")
-elif jugador2>jugador1 and jugador2>jugador3:
-    print("El jugador 2 tuvo el golpe mas fuerte, que fue de:", jugador2, "metros")
-else:
-    print("El jugador 3 tuvo el golpe mas fuerte, que fue de:", jugador3, "metros")
+#tiempo de espera
 
+#Killer instinc 
+
+#Dos peleadores se piden al inicio de la pelea
+#cada peleador inicia con 100 de HP
+#se debe hacer una pelea por turnos
+#y cada golpe varia entre 7 y 18
+#se termina el match cuando uno de los 2
+#tiene su HP menor o igual a 0
+#BONUS: mostrar barras de energia de cada peleador 
+
+jugador1=input("Ingrese el nombre del jugador 1:")
+jugador2=input("Ingrese el nombre del jugador 2:")
+j1=100
+j2=100
+turno=1
+while j1 > 0 and j2 > 0:
+    punch = random.randint(7,18)
+    if turno % 2 == 1:
+        j2 -= punch
+        if j2 < 0:
+            j2 = 0
+        print("Turno uno")
+        print(jugador1, "hizo", punch, "de daño")
+        print(jugador2, "tiene", j2, "de HP")
+    else:
+        j1 -= punch
+        if j1 < 0:
+            j1 = 0
+        print("Turno dos")
+        print(jugador2, "hizo", punch, "de daño")
+        print(jugador1, "tiene", j1, "de HP")
+    turno += 1
+    time.sleep(1)
+
+if j1 <= 0:
+    print(jugador2, "ganó")
+elif j2 <= 0:
+    print(jugador1, "ganó")
+
+#turnos:
+# turno=1
+# if turno%2==0:
+#     print("Turno uno")
+# else:
+#     print("Turno dos")
