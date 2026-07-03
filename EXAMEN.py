ca01787db296f3811bc6306a24eb3cbaff246724
fcheu <fabian.cheuquerojas@gmail.com>
2026-07-03 11:56:20 -0400

    ultimaclase

--- EXAMEN.py
+++ EXAMEN.py
@@ -40,4 +40,126 @@ def autosVendidos_marca(d, marca):
     print(f'El número total de autos vendidos de la marca {marca.upper()} es {total}')
 
 def busqueda_por_anio(anio_min, anio_max):
+    elementos_encontrados = []
+    for id_auto, datos in autos.items():
+        marca = datos[0]
+        modelo = datos[1]
+        anio = datos[2]
+        if anio_min <= anio <= anio_max:
+            if operaciones[id_auto][1] != 'Pendiente':
+                elementos_encontrados.append(f'{marca} {modelo} -- {id_auto}')
     
+    if elementos_encontrados:
+        elementos_encontrados.sort()
+        print(elementos_encontrados)
+    else:
+        print('No se han encontrado elementos')
+
+def actualizar_fecha_venta(id_auto, nueva_fecha):
+    if id_auto in operaciones:
+        operaciones[id_auto][-1]=nueva_fecha
+        return True
+    else:
+        return False
+while True:
+    id=input("Ingrese el id del auto: ")
+    fecha=input("Ingrese la fecha de venta: ")
+        
+    if actualizar_fecha_venta(id,fecha):
+        print("Exito, nueva fecha de venta actualizada")
+    else: 
+        print("Metio mal las manos")
+    next=input("¿Desea actualizar otro vehiculo (s/n)?")
+    if next.lower()=="n":
+        break
+
+def validaString(m):
+    if m=="" and m==" ": 
+        return True
+    else: 
+        return False
+    
+def validaAño(a):
+    if a<1900:
+        return True
+    else: 
+        return False
+    
+def validaRanking(r):
+    if r>=1 and r<=5:
+        return False
+    else: 
+        return True
+
+def nuevoAuto(d):
+    id=input("Ingrese el ID: ")
+    if validaString(id):
+        print("Dato invalido")
+        return 
+    marca=input("Ingrese la marca: ")
+    if validaString(marca):
+        print("Dato invalido")
+        return 
+    modelo=input("Ingrese el modelo: ")
+    if  validaString(modelo):
+        print("Dato invalido")
+        return
+    año=int(input("Ingrese el año: "))
+    if validaAño(año):
+        print("El año debe ser mayor a 1900")
+        return 
+    ranking=int(input("Ingrese el ranking: "))
+    if validaRanking(ranking):
+        print("El ranking debe estar entre 1 y 5")
+        return 
+    fecha=input("Ingrese la fecha (dd-mm-yyyy): ")
+    if validaString(fecha):
+        return
+    autos[id]=[marca, modelo, año, ranking]
+    operaciones[id]=[fecha, 'Pendiente']
+mostrarAutos(autos)
+nuevoAuto(autos)
+mostrarAutos(autos)
+def eliminar_auto(id_auto):
+    if id_auto in autos:
+        del autos[id_auto]
+        del operaciones[id_auto]
+        return True
+    else: 
+        return False
+
+#Hacer un menu con todas las funciones que hicimos en clase
+#debe tener manejo de errores (try-except)
+def menuAutos():
+    while True:
+        try:
+            print("-"*60)
+            print("1.- Mostrar autos")
+            print("2.- Ver autos vendidos")
+            print("3.- Ver autos vendidos por marca")
+            print("4.- Buscar auto por año")
+            print("5.- Actualizar fecha de venta")
+            print("6.- Agregar nuevo auto")
+            print("7.- Salir")
+            op=int(input("Seleccione una opcion: "))
+            match op:
+                case 1:
+                    mostrarAutos()
+                case 2:
+                    autosVendidos()
+                case 3:
+                    autosVendidos_marca()
+                case 4:
+                    busqueda_por_anio()
+                case 5:
+                    actualizar_fecha_venta() 
+                case 6: 
+                     busca = input("¿Qué color busca?: ")
+                     resultado = dispColores(pinturas, busca)
+                     print(resultado)
+                case 7:
+                    print("Saliendo...")
+                case _:
+                    print("Opcion invalida")
+        except Exception as e:
+            print("Error: ", e)
